250cc8dd45d083842f0a1dad2befa1fca0e007cf
minjun kim <minzzzun@gmail.com>
2024-11-10 17:17:44 +0900

    ignore 연습

---
+++ makeGitIgnore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  makeGitIgnore
+//
+//  Created by 김민준 on 11/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
